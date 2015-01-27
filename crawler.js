0374d4f82d0b0bfd692e5f52813defccb6e4a0c5
Jason Park <jason.park@laterooms.com>
2015-01-27 14:40:41 +0800

    Unify control flow module to async

--- crawler.js
+++ crawler.js
@@ -3,42 +3,34 @@
 var mongo = require('mongodb'),
     jsdom = require('jsdom'),
     async = require('async'),
-    Twitter = require('easy-tweet'),
-    Q = require('q');
+    Twitter = require('easy-tweet');
 
-var promiseCallback = function (reject, resolve) {
-  return function (err, result) {
-    if(err) reject(err);
-    else resolve(result);
+var getMongoCollection = function (url, collectionName) {
+  return function (callback) {
+    mongo.MongoClient.connect(url, { native_parser:true }, function(err, db) {
+      db.collection(collectionName, callback);
+    });
   };
 };
 
-var getMongoCollection = function (url, collectionName) {
-  return Q.Promise(function (resolve, reject) {
-    mongo.MongoClient.connect(url, { native_parser:true }, function(err, db) {
-      db.collection(collectionName, promiseCallback(reject, resolve));
-    });
-  });
-};
-
 var loadWindow = function (url) {
-  return Q.Promise(function (resolve, reject) {
+  return function (callback) {
     jsdom.env({
       url: url,
       done: function (err, window) {
-        if (err) reject(err);
+        if (err) callback(err);
         else {
           jsdom.jQueryify(window, 'http://code.jquery.com/jquery-2.1.1.js', function () {
-            resolve(window);
+            callback(null, window);
           });
         }
       }
     });
-  });
+  };
 };
 
 var queryPages = function (pages) {
-  return Q.Promise(function (resolve, reject) {
+  return function (callback) {
     async.mapSeries(pages, function (page, done) {
       loadWindow(page.url).then(function (window) {
         page.query(window, function (items) {
@@ -46,63 +38,44 @@ var queryPages = function (pages) {
           done(null, items)
         });
       })
-    }, function (err, results) {
-      if(err) reject(err);
-      else resolve(results);
-    });
-  });
-};
-
-var getItemCount = function (collection, identifier) {
-  return Q.Promise(function (resolve, reject) {
-    collection.findOne({ identifier: identifier }, promiseCallback(resolve, reject));
-  })
+    }, callback);
+  };
 };
 
 var saveAndTweet = function (collection, twitterClient, item) {
-  return Q.Promise(function (resolve, reject) {
-
+  return function (callback) {
     collection.insert(item, function(err, result){
-      if(err) {
-        reject(err);
-      } else {
-        twitterClient.tweet(item.content);
-        resolve();
-      }
+      twitterClient.tweet(item.content);
+      callback(err);
     });
-  });
+  };
 };
 
-var processQueryResults = function (results, collection, twitterClient) {
-  return Q.Promise(function(resolve, reject) {
-    var processResult = function (item, done) {
-      var id = item.identifier;
+var processQueryResults = function (err, results) {
+  var processResult = function (item, done) {
+    var id = item.identifier;
 
-      collection.findOne({ identifier: id }, function (err, doc) {
-        if(!doc) {
-          saveAndTweet(collection, twitterClient, item).then(done);
-        } else {
-          done();
-        }
-      });
-    };
-
-    // iterate over query results from a page
-    var pageResultIterator = function (pageItems, done){
-      async.each(pageItems, processResult, done);
-    };
-
-    var closeDB = function (err) {
-      if(err) {
-        reject(err);
+    collection.findOne({ identifier: id }, function (err, doc) {
+      if(!doc) {
+        saveAndTweet(collection, twitterClient, item).then(done);
+      } else {
+        done();
       }
-      collection.db.close();
-      resolve();
-    }
+    });
+  };
 
-    // iterate over query results
-    async.each(results, pageResultIterator, closeDB);
-  });
+  // iterate over query results from a page
+  var pageResultIterator = function (pageItems, done){
+    async.each(pageItems, processResult, done);
+  };
+
+  var closeDB = function (err) {
+    collection.db.close();
+    callback(err);
+  }
+
+  // iterate over query results
+  async.each(results, pageResultIterator, closeDB);
 };
 
 // options = {
@@ -147,10 +120,9 @@ Crawler.prototype.run = function() {
 
   var getQueryResults = queryPages(options.pages);
   var getCollection = getMongoCollection(options.mongoDB.url, options.mongoDB.collection);
-  var getTwitterClient = Q.fcall(function() { return twitterClient; });
+  var getTwitterClient = function() { return twitterClient; };
 
-  return Q.all([ getQueryResults, getCollection, getTwitterClient ])
-          .spread(processQueryResults);
+  async.parallel([ getQueryResults, getCollection, getTwitterClient ], processQueryResults);
 };
 
 module.exports = Crawler;
